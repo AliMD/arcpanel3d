84b9c6dab7ca7719ff4ace760206f4e64f7e7f97
AliGH <aghasizadeh70@gmail.com>
2012-08-23 12:08:25 +0430

    fix project for stylus

--- index.php
+++ index.php
@@ -3,9 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<title>Arc Panel 3D</title>
-	<link type="text/css" rel="stylesheet" href="styles/reset.styl" />
-	<link type="text/css" rel="stylesheet" href="styles/1styles.styl" />
-	<link type="text/css" rel="stylesheet" href="styles/layout.styl">
+	<link type="text/css" rel="stylesheet" href="style.css">
 	<!--[if IE]>
 		<script type="text/javascript" src="scripts/html5shiv-printshiv.js"></script>
 	<![endif]-->
@@ -15,5 +13,23 @@
 	<script type="text/javascript" src="scripts/script.js"></script>
 </head>
 <body>
+<header></header>
+<section class="aside">
+	<div class="top-bg"></div>
+	<nav class="nav">
+		<div class="logo mla mra">
+			<h1>Arc Panel</h1>
+		</div>
+		<menu class="mla mra">
+			<li><a href="#">home</a></li>
+			<li><a href="#">about</a></li>
+			<li><a href="#">portfolio</a></li>
+			<li><a href="#">our stores</a></li>
+			<li><a href="#">online shop</a></li>
+			<li><a href="#">contact us</a></li>
+		</menu>		
+	</nav>
+	<div class="bot-bg"></div>
+</section>
 </body>
 </html>
