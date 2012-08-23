23a18d5a4cf716a8555dbbbb4ad17ab4a5e5a2aa
AliGH <aghasizadeh70@gmail.com>
2012-08-23 11:35:18 +0430

    start the project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en-US">
+<head>
+	<meta charset="utf-8" />
+	<title>Arc Panel 3D</title>
+	<link type="text/css" rel="stylesheet" href="styles/reset.styl" />
+	<link type="text/css" rel="stylesheet" href="styles/1styles.styl" />
+	<link type="text/css" rel="stylesheet" href="styles/layout.styl">
+	<!--[if IE]>
+		<script type="text/javascript" src="scripts/html5shiv-printshiv.js"></script>
+	<![endif]-->
+	<script type="text/javascript">
+		document.write('<script type="text/javascript" src=scripts/' + ('__proto__' in {} ? 'zepto' : 'jquery') + '.js><\/script>');
+	</script>
+	<script type="text/javascript" src="scripts/script.js"></script>
+</head>
+<body>
+</body>
+</html>
