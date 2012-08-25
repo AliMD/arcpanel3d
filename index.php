e5514f41d48dff57c1cd44d8eaa178e85602e8e6
AliGH <aghasizadeh70@gmail.com>
2012-08-26 03:34:34 +0430

    marker is done too

--- index.php
+++ index.php
@@ -28,14 +28,17 @@
 	</nav>
 	<div class="bot-bg"></div>
 </section>
-<menu class="right-panel"> <!--should coded with php to load images-->
-	<li><a href="#"><img src="images/right-side/1.png" alt="image1" tooltip="" /></a></li>
-	<li><a href="#"><img src="images/right-side/2.png" alt="image2" tooltip="" /></a></li>
-	<li><a href="#"><img src="images/right-side/3.png" alt="image3" tooltip="" /></a></li>
-	<li><a href="#"><img src="images/right-side/4.png" alt="image4" tooltip="" /></a></li>
-	<li><a href="#"><img src="images/right-side/5.png" alt="image5" tooltip="" /></a></li>
-	<li><a href="#"><img src="images/right-side/6.png" alt="image6" tooltip="" /></a></li>
-</menu>
+<aside class="right-aside"><!--MONDAM CHE CLASSI BEZARAM :D-->
+	<menu class="right-panel"> <!--should coded with php to load images-->
+		<li><a href="#"><img src="images/right-side/1.png" alt="image1" tooltip="" /></a></li>
+		<li><a href="#"><img src="images/right-side/2.png" alt="image2" tooltip="" /></a></li>
+		<li><a href="#"><img src="images/right-side/3.png" alt="image3" tooltip="" /></a></li>
+		<li><a href="#"><img src="images/right-side/4.png" alt="image4" tooltip="" /></a></li>
+		<li><a href="#"><img src="images/right-side/5.png" alt="image5" tooltip="" /></a></li>
+		<li><a href="#"><img src="images/right-side/6.png" alt="image6" tooltip="" /></a></li>
+	</menu>
+	<div class="marker"></div>
+</aside>
 <section class="slideshow">
 	<div class="noise"></div>
 	<div class="backimg">
